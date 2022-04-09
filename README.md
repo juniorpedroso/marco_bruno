138a589773b8fc17c5611712c654429395c3d45d
juniorpedroso <sintasebemagora@gmail.com>
2022-04-08 23:43:37 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# marco_bruno
+ Curso de HTML 
---
+++ aula005_relative_position/position.js
@@ -0,0 +1,14 @@
+const $elementExampleFirst = document.querySelector('.elementExample_first')
+
+const $elementExampleSecond = document.querySelector('.elementExample_second')
+   
+$elementExampleFirst.addEventListener('click', function (event) {
+    this.classList.toggle('elementExample_isActive')
+    $elementExampleSecond.classList.add('elementExample_isActive')
+})
+
+$elementExampleSecond.addEventListener('click', function (event) {
+    this.classList.toggle('elementExample_isActive')
+    $elementExampleFirst.classList.add('elementExample_isActive')
+}) 
+
